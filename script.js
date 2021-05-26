7763ed3c61bd4ff0cd862d30b9ef38996a97cf54
NandhaKumar.K <82039185+Kum4tor@users.noreply.github.com>
2021-05-26 22:56:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function display(){
+    var firstName = document.getElementById("fn").value;
+    var lastname = document.getElementById("ln").value;
+    document.getElementById("result").innerHTML="Thank you for showing your support "+firstName+" "+lastname+". Explore more petitions!!"
+
+}
